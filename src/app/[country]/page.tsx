87606429e8e9dac9ed881cb619cfe921521aa7ca
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2025-06-10 18:24:40 +0200

    add ednf

---
+++ src/app/[country]/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function CountryPage() {
+    return (
+        <>
+            <h1>Country Page</h1>
+            <Link href="/" >Return</Link>
+        </>
+    )
+}
