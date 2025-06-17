e50b075429020f0712cbaaaffb27b8cbfcc78954
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2025-06-17 19:54:18 +0200

    update type

--- src/components/client/GuessCountry.tsx
+++ src/components/client/GuessCountry.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { SearchAutocomplete } from "@/parts/utils";
 
-export default function GuessCountry({ countries }: { countries: Array<t_Country_Table_Record> }) {
+export default function GuessCountry({ countries }: { countries: Array<T_Country_Single> }) {
     return (
         <SearchAutocomplete countries={countries} />
     )
