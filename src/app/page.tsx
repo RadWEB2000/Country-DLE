6c650aaee79e81601a7e62926784be5bc483ed4d
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2025-06-18 20:23:38 +0200

    remove unused code lines, and hashed comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import { CountrySelectedProvider } from '@/providers';
-import getAllCountries from '@/lib/functions/getAllCountries';
-import getSingleCountry from '@/lib/functions/getSingleCountry';
 import Table from '@/components/utils/Table';
 import { SearchAutocomplete } from '@/components/utils';
-import { getWinningCountry } from '@/lib/functions';
+import { getAllCountries, getWinningCountry } from '@/lib/functions';
 
 export default async function GuessCountryPage() {
 
   const countries = (await getAllCountries());
-  const win_country = await getSingleCountry('argentina');
   const win = await getWinningCountry();
 
-  console.log('win', win)
+  // console.log('win', win)
 
   return (
     <CountrySelectedProvider>
