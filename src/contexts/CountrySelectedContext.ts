3aa9b4fdb970451e65c34f47b4ed0c4789752e5f
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2025-07-01 07:26:41 +0200

    finishing

--- src/contexts/CountrySelectedContext.ts
+++ src/contexts/CountrySelectedContext.ts
@@ -3,9 +3,13 @@ import { createContext } from "react";
 
 const CountrySelectedContext = createContext<{
     countries: Array<T_Country_Single>,
-    updateCountries: (country: T_Country_Single) => void
+    updateCountries: (country: T_Country_Single) => void,
+    isWin: boolean;
+    updateWinStatus: (status: boolean) => void;
 }>({
     countries: [],
+    isWin: false,
+    updateWinStatus: () => { },
     updateCountries: () => { }
 })
 
